8c9508926fa06afe822fed835ffa550a5c0e832b
AngievR05 <159420956+AngievR05@users.noreply.github.com>
2025-05-08 09:37:09 +0200

    updated app.js

--- creative-auth-bartender/client/src/App.js
+++ creative-auth-bartender/client/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginGame from './pages/LoginGame';
-import Register from './pages/Register';
-import Login from './pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Welcome from './pages/Welcome'
+import GameAuth from './pages/GameAuth'
+import GameSuccess from './pages/GameSuccess'
+import GameFailure from './pages/GameFailure'
+import AccessibilityLogin from './pages/AccessibilityLogin'
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginGame />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Welcome />} />
+        <Route path="/game" element={<GameAuth />} />
+        <Route path="/success" element={<GameSuccess />} />
+        <Route path="/failure" element={<GameFailure />} />
+        <Route path="/accessibility-login" element={<AccessibilityLogin />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+
