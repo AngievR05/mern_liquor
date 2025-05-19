dffe11de82b88449d2a95510b3399fe80c644871
AngievR05 <159420956+AngievR05@users.noreply.github.com>
2025-05-19 15:05:06 +0200

    server.js added

--- creative-auth-bartender/server/server.js
+++ creative-auth-bartender/server/server.js
@@ -1,46 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import userRoutes from './routes/userRoutes.js';
+import gameRoutes from './routes/gameRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/game', gameRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-mongoose.connect(process.env.MONGODB_URI, {
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch((err => console.log(err)));
-
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const cors = require('cors');
-// const connectDB = require('./config/db');
-
-// dotenv.config();
-// connectDB();
-
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Routes
-// app.use('/api/auth', require('./routes/authRoutes'));
-// app.use('/api/game', require('./routes/gameRoutes'));
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB connected');
+  app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+}).catch(err => console.log(err));
