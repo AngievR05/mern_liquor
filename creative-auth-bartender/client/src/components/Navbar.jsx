f15b10a4db3d8267e333a7c94da55cf665b07b13
AngievR05 <159420956+AngievR05@users.noreply.github.com>
2025-05-22 12:43:59 +0200

    links added

--- creative-auth-bartender/client/src/components/Navbar.jsx
+++ creative-auth-bartender/client/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/Navbar.css";
 import LogoNoText from "../assets/Logo-no-text.svg";
+import Store from "../pages/Store";
+import About from "../pages/About";
 
 const Navbar = ({ onLoginClick }) => {
   return (
@@ -10,8 +12,8 @@ const Navbar = ({ onLoginClick }) => {
         <h3>The Drunken Giraffe</h3>
       </div>
       <div className="navLinksMiddle">
-        <a href="#">About</a>
-        <a href="#">Store</a>
+        <a href=<About />>About</a>
+        <a href=<Store />>Store</a>
       </div>
       <div className="navLinksRight">
         <a href="#" onClick={e => { e.preventDefault(); onLoginClick && onLoginClick(); }}>Login</a>
