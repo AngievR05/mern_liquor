ed8aad4081ebdec2f84d06265494de1b338ab45a
AngievR05 <159420956+AngievR05@users.noreply.github.com>
2025-05-22 12:50:19 +0200

    navbar updated
    cart icon added and navlinks done

--- creative-auth-bartender/client/src/components/Navbar.jsx
+++ creative-auth-bartender/client/src/components/Navbar.jsx
@@ -1,9 +1,7 @@
 import React from "react";
+import { FaShoppingCart } from "react-icons/fa";  // Import cart icon
 import "../styles/Navbar.css";
 import LogoNoText from "../assets/Logo-no-text.svg";
-import Store from "../pages/Store";
-import About from "../pages/About";
-
 const Navbar = ({ onLoginClick }) => {
   return (
     <nav>
@@ -11,14 +9,20 @@ const Navbar = ({ onLoginClick }) => {
         <img src={LogoNoText} alt="Logo" className="logo" />
         <h3>The Drunken Giraffe</h3>
       </div>
+
       <div className="navLinksMiddle">
-        <a href=<About />>About</a>
-        <a href=<Store />>Store</a>
+        <a href="/about">About</a>
+        <a href="/store">Store</a>
       </div>
+
       <div className="navLinksRight">
-        <a href="#" onClick={e => { e.preventDefault(); onLoginClick && onLoginClick(); }}>Login</a>
+        <a href="#" onClick={e => { e.preventDefault(); onLoginClick && onLoginClick(); }} > Login</a>
+        <div className="cart-icon" style={{ marginTop: "8px", cursor: "pointer" }}>
+          <a href="/cart"><FaShoppingCart size={24} /></a>
+        </div>
       </div>
     </nav>
   );
-}
+};
+
 export default Navbar;
